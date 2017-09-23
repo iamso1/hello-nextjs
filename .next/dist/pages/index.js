af9cd8ee052080de68b8e7ffe14d3425a32d7885
Hsinhan <bmhiamso1@gmail.com>
2017-09-24 01:16:34 +0800

    temp

--- .next/dist/pages/index.js
+++ .next/dist/pages/index.js
@@ -12,10 +12,6 @@ var _todo = require('../components/todo');
 
 var _todo2 = _interopRequireDefault(_todo);
 
-var _hero = require('../components/hero');
-
-var _hero2 = _interopRequireDefault(_hero);
-
 var _layout = require('../components/layout');
 
 var _layout2 = _interopRequireDefault(_layout);
@@ -28,17 +24,17 @@ exports.default = function () {
   return _react2.default.createElement('div', {
     __source: {
       fileName: _jsxFileName,
-      lineNumber: 6
+      lineNumber: 5
     }
   }, _react2.default.createElement(_layout2.default, {
     __source: {
       fileName: _jsxFileName,
-      lineNumber: 7
+      lineNumber: 6
     }
-  }, _react2.default.createElement(_todo2.default, { title: 'this is idb todo list', __source: {
+  }, _react2.default.createElement(_todo2.default, { title: '\u6D77\u7522\u4FF1\u6A02\u90E8\u8A0E\u8AD6\u7248', __source: {
       fileName: _jsxFileName,
-      lineNumber: 8
+      lineNumber: 7
     }
   })));
 };
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInBhZ2VzXFxpbmRleC5qcyJdLCJuYW1lcyI6WyJUb2RvIiwiSGVybyIsIkxheW91dCJdLCJtYXBwaW5ncyI6Ijs7Ozs7Ozs7OztBQUFBLEFBQU87Ozs7QUFDUCxBQUFPOzs7O0FBQ1AsQUFBTyxBQUVQOzs7Ozs7OztrQkFBZSxZQUFBO3lCQUNiLGNBQUE7O2dCQUFBO2tCQUFBLEFBQ0U7QUFERjtBQUFBLEdBQUEsa0JBQ0UsQUFBQzs7Z0JBQUQ7a0JBQUEsQUFDRTtBQURGO0FBQUEscUJBQ0UsQUFBQyxnQ0FBSyxPQUFOLEFBQVk7Z0JBQVo7a0JBSFMsQUFDYixBQUNFLEFBQ0U7QUFBQTs7QUFITiIsImZpbGUiOiJpbmRleC5qcz9lbnRyeSIsInNvdXJjZVJvb3QiOiJEOi93b3Jrc3BhY2UvUGxheVByb2plY3QvaGVsbG9OZXh0In0=
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInBhZ2VzXFxpbmRleC5qcyJdLCJuYW1lcyI6WyJUb2RvIiwiTGF5b3V0Il0sIm1hcHBpbmdzIjoiOzs7Ozs7Ozs7O0FBQUEsQUFBTzs7OztBQUNQLEFBQU8sQUFFUDs7Ozs7Ozs7a0JBQWUsWUFBQTt5QkFDYixjQUFBOztnQkFBQTtrQkFBQSxBQUNFO0FBREY7QUFBQSxHQUFBLGtCQUNFLEFBQUM7O2dCQUFEO2tCQUFBLEFBQ0U7QUFERjtBQUFBLHFCQUNFLEFBQUMsZ0NBQUssT0FBTixBQUFZO2dCQUFaO2tCQUhTLEFBQ2IsQUFDRSxBQUNFO0FBQUE7O0FBSE4iLCJmaWxlIjoiaW5kZXguanM/ZW50cnkiLCJzb3VyY2VSb290IjoiRDovd29ya3NwYWNlL1BsYXlQcm9qZWN0L2hlbGxvTmV4dCJ9
